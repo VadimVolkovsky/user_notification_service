f2a1b186b0af32fd037b243487cc288764531098
VadimVolkovsky <vadimas29@gmail.com>
2024-08-14 21:25:15 +0300

    доработки на вебинаре

--- src/api/v1/notifications.py
+++ src/api/v1/notifications.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends
 
 from services.notification_service import NotificationService, get_notification_service
-from src.schemas.api_schemas import NotificationToSend
+from src.schemas.api_schemas import Notification
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ router = APIRouter()
     status_code=HTTPStatus.OK,
 )
 async def add_notification(
-        notification: NotificationToSend,
+        notification: Notification,
         notification_service: NotificationService = Depends(get_notification_service),
 ):
     """Эндпоинт для добавления нового уведомления в очередь"""
