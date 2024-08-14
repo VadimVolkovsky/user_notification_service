884564c47abd0e8cdda510a424e51c3e1b34a758
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-08-15 01:27:21 +0300

    added task to send new films

--- scheduler/src/notification/tasks.py
+++ scheduler/src/notification/tasks.py
@@ -1,19 +1,48 @@
+import logging
+
 import requests
-from celery import shared_task
+from celery.schedules import crontab
 
-@shared_task
-def fetch_and_send_movies():
-    # Получение списка новых фильмов с исходного API
-    response = requests.get('https://source-api.example.com/new-movies')
+from scheduler.src.celery import app
+from scheduler.src.notification.evevt_manager import TaskManager
+from scheduler.src.notification.models import NotificationType, Chanel, NotificationTemplate, Notification
+from scheduler.src.notification.serializers import notification_serializer
 
-    if response.status_code == 200:
-        movies = response.json()
+logger = logging.getLogger(__name__)
 
-        # Отправка списка на целевой API
-        destination_response = requests.post('https://destination-api.example.com/movies', json=movies)
-        if destination_response.status_code == 200:
-            return 'Фильмы успешно отправлены!'
-        else:
-            return f'Не удалось отправить фильмы: {destination_response.status_code}'
-    else:
-        return f'Не удалось получить фильмы: {response.status_code}'
+
+@app.task
+def task_get_new_films(task_manager: TaskManager = TaskManager()):
+    try:
+        new_movies = task_manager.get_new_films()
+        user_group = task_manager.get_users_subscribers()
+        notif_type = NotificationType.NEW_FILM.name
+        template = NotificationTemplate.objects.get(type=notif_type)
+
+        notification = Notification.objects.create(
+            title="New films",
+            type=NotificationType.NEW_FILM.name,
+            channel=Chanel.EMAIL.name,
+            user_group=user_group,
+            template=template,
+            context=dict(film_list=new_movies)
+        )
+        notification_json = notification_serializer(notification)
+
+        response = requests.post('http://app:8000/api/v1/add_event', json=notification_json)
+        logging.info(f'[GET_NEW FILM] Все ок {response.json}')
+    except NotificationTemplate.DoesNotExist:
+        logging.error(f'[GET_NEW FILM] NotificationTemplate с типом  {notif_type} не найден')
+        raise Exception('NotificationTemplate с типом  {notif_type} не найден')
+    except Exception as e:
+        logging.error(f'[GET_NEW FILM] Видимо что-то случилось {e}')
+        raise Exception(e)
+
+
+@app.on_after_configure.connect
+def periodic_task_notification_new_movies(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute=0, hour=18, day_of_week='fri'),  # 18:00 по пятницам,
+        task_get_new_films.s(),
+        name='New movies',
+    )
