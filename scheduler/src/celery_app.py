eadc2055dfc85868436404e5013443211c1438d6
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-08-15 21:25:12 +0300

    update celery config

--- scheduler/src/celery_app.py
+++ scheduler/src/celery_app.py
@@ -2,11 +2,13 @@ import os
 
 from celery import Celery
 
+from config.components.celery_settings import CELERY_BROKER_URL
+
 # Установка переменной окружения для настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Создание экземпляра приложения Celery
-app = Celery('.')
+app = Celery('celery_app', broker=CELERY_BROKER_URL)
 
 # Загрузка настроек Django в Celery
 app.config_from_object('django.conf:settings', namespace="CELERY")
--- scheduler/src/notification/tasks.py
+++ scheduler/src/notification/tasks.py
@@ -3,11 +3,10 @@ import logging
 import requests
 from celery import shared_task
 from celery.schedules import crontab
-from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
 from celery_app import app
 from notification.evevt_manager import TaskManager
-from notification.models import NotificationType, Chanel, NotificationTemplate, Notification
+from notification.models import NotificationType, Chanel, Notification
 from notification.serializers import notification_serializer
 
 logger = logging.getLogger(__name__)
@@ -19,11 +18,11 @@ def task_get_new_films(task_manager: TaskManager = TaskManager()):
         new_movies = task_manager.get_new_films()
         recipients = task_manager.get_users_subscribers()
         notif_type = NotificationType.NEW_FILM.name
-        template = NotificationTemplate.objects.get(type=notif_type)
+        # template = NotificationTemplate.objects.get(type=notif_type)
 
         notification = Notification.objects.create(
             title="New films",
-            type=NotificationType.NEW_FILM.name,
+            type=notif_type,
             channel=Chanel.EMAIL.name,
             recipients=recipients,
             # template=template,
@@ -33,9 +32,9 @@ def task_get_new_films(task_manager: TaskManager = TaskManager()):
 
         response = requests.post('http://app:8000/api/v1/add_notification', json=notification_json)
         logging.info(f'[GET_NEW FILM] Все ок {response.json}')
-    except NotificationTemplate.DoesNotExist:
-        logging.error(f'[GET_NEW FILM] NotificationTemplate с типом  {notif_type} не найден')
-        raise Exception('NotificationTemplate с типом  {notif_type} не найден')
+    # except NotificationTemplate.DoesNotExist:
+    #     logging.error(f'[GET_NEW FILM] NotificationTemplate с типом  {notif_type} не найден')
+    #     raise Exception('NotificationTemplate с типом  {notif_type} не найден')
     except Exception as e:
         logging.error(f'[GET_NEW FILM] Видимо что-то случилось {e}')
         raise Exception(e)
@@ -49,25 +48,25 @@ def task_get_new_episodes_of_series(task_manager: TaskManager = TaskManager()):
         for episode in new_episodes:
             user_group = episode.pop('users')
             episode_number = episode.pop('episode_number')
-            notif_type = NotificationType.NEW_SERIES.name
+            notif_type = NotificationType.NEW_EPISODE.name
             # template = NotificationTemplate.objects.get(type=notif_type)
             message = f'Вышел новый эпизод {episode_number}  сериала {episode['title']}'
 
             notification = Notification.objects.create(
                 title='New episode of series',
-                type=NotificationType.NEW_FILM.name,
+                type=notif_type,
                 channel=Chanel.EMAIL.name,
                 user_group=user_group,
-                template=template,
+                # template=template,
                 context=dict(film_list=[episode,], episode_number=episode_number, message=message)
             )
             notification_json = notification_serializer(notification)
 
             response = requests.post('http://app:8000/api/v1/add_notification', json=notification_json)
             logging.info(f'[GET_NEW_EPISODE] Все ок {response.json}')
-    except NotificationTemplate.DoesNotExist:
-        logging.error(f'[GET_NEW GET_NEW_EPISODE] NotificationTemplate с типом {notif_type} не найден')
-        raise Exception('NotificationTemplate с типом  {notif_type} не найден')
+    # except NotificationTemplate.DoesNotExist:
+    #     logging.error(f'[GET_NEW GET_NEW_EPISODE] NotificationTemplate с типом {notif_type} не найден')
+    #     raise Exception('NotificationTemplate с типом  {notif_type} не найден')
     except Exception as e:
         logging.error(f'[GET_NEW GET_NEW_EPISODE] Видимо что-то случилось {e}')
         raise Exception(e)
@@ -76,16 +75,23 @@ def task_get_new_episodes_of_series(task_manager: TaskManager = TaskManager()):
 @app.on_after_finalize.connect
 def periodic_task_notification_new_movies(sender, **kwargs):
     sender.add_periodic_task(
-        crontab(minute=0, hour=18, day_of_week='fri'),  # 18:00 по пятницам
+        crontab(minute='0', hour='18', day_of_week='fri'),  # 18:00 по пятницам
         task_get_new_films.s(),
         name='New movies friday evening',
     )
 
-
+# @app.on_after_finalize.connect
+# def periodic_task_notification_new_movies(sender, **kwargs):
+#     sender.add_periodic_task(
+#         crontab(minute='1'),  # 18:00 по пятницам
+#         task_get_new_films.s(),
+#         name='New movies friday evening',
+#     )
+#
 @app.on_after_finalize.connect
 def periodic_task_notification_new_episodes(sender, **kwargs):
-    sender.task_get_new_episodes_of_series(
-        crontab(minute=0, hour=12),  # 12:00
+    sender.add_periodic_task(
+        crontab(minute='0', hour='12'),  # 12:00
         task_get_new_episodes_of_series.s(),
         name='New episodes of series daily',
     )
