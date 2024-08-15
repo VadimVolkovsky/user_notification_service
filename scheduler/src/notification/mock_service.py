6cd70b96d6a913cdcfdb648c578ac3b193b20bbe
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-08-15 11:17:20 +0300

    added fake get series

--- scheduler/src/notification/mock_service.py
+++ scheduler/src/notification/mock_service.py
@@ -2,23 +2,40 @@ import uuid
 from faker import Faker
 from datetime import datetime, timedelta
 
+fake = Faker()
+
 
 class MockService:
     def generate_new_films(self) -> list[dict]:
-        fake = Faker()
 
         movies = []
         for _ in range(5):
             movie = {
                 "id": str(uuid.uuid4()),
-                "title": fake.sentence(nb_words=3),
+                "title": fake.catch_phrase(),
                 "release": (datetime.now() - timedelta(days=fake.random_int(min=0, max=7))).strftime('%Y-%m-%d')
             }
             movies.append(movie)
-            return movies
+        return movies
 
     def get_bookmarks(self) -> iter:
         pass
 
     def get_new_films(self) -> list[dict]:
         return self.generate_new_films()
+
+    def generate_new_episodes_of_series(self) -> list[dict]:
+        Faker.seed(0)
+
+        series_list = []
+        for _ in range(5):
+            series = {
+                "id": str(uuid.uuid4()),
+                "title": fake.catch_phrase(),
+                "release": (datetime.now() - timedelta(days=fake.random_int(min=0, max=7))).strftime('%Y-%m-%d'),
+                'episode_number': fake.random_int(min=0, max=20),
+                "users": [str(uuid.uuid4()) for _ in range(fake.random_int(min=1, max=5))]
+            }
+            series_list.append(series)
+
+        return series_list
