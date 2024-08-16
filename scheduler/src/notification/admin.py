63e21e18ad2781cbb97136538aba587cb4b00be9
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-08-16 17:27:02 +0300

    - added notifications manual sending, - added response.raise_for_status() to tasks

--- scheduler/src/notification/admin.py
+++ scheduler/src/notification/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Notification
 
+from tasks import task_send_admin_notification
+
 
 @admin.register(Notification)
 class NotificationTaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'type', 'created_at')
     search_fields = ('title', 'recipients')
+    actions = ['send_notifications']
+
+    def send_notifications(self, request, queryset):
+        for notification in queryset:
+            task_send_admin_notification.delay(notification.id)
+            notification.status = 'Sent'
+            notification.save()
